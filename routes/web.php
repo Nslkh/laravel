ff699b09da7fecd1191b3d94ec2bb836c048a312
Nslkh <olluzran@gmail.com>
2021-03-31 00:40:52 +0500

    method, route creted for posts

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PagesController::class, 'index']);
 
 Route::get('/about', [PagesController::class, 'about']);
+
+// Posts Endpoint
+Route::get('/posts', [PostController::class, 'index']);
