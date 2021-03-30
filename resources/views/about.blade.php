15f37082f65fc962c40fef2c4bd46b599e3be58f
Nslkh <olluzran@gmail.com>
2021-03-30 17:06:37 +0500

    unless, empty, isset usage

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,3 +5,20 @@
 @else 
     <h2>All conditions are wrong!</h2>
 @endif
+
+
+@unless (empty($name))
+    <h2>Name is not empty</h2>
+@endunless
+
+{{-- @if (!empty($name))
+    <h2>Name is not</h2>
+@endif --}}
+
+@empty(!$name)
+    <h2>Name is empty</h2>
+@endempty
+
+@isset($name)
+    <h2>Name has been set</h2>
+@endisset
