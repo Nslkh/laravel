8407d575d767f6fd5cf6eef9ac3e61fb4a54d5ec
Nslkh <olluzran@gmail.com>
2021-03-30 21:18:48 +0500

    Blade Directives / switch

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,24 +1,19 @@
-@if (5 > 10)
-    <p> 5 is lower than 10</p>
-@elseif(5 == 10)
-    <p> 5 is indeed lower than 10</p>
-@else 
-    <h2>All conditions are wrong!</h2>
-@endif
+{{-- 
+    Comparing Multiple possible conditions 
+    Multiple values that may require the same code
+--}}
 
-
-@unless (empty($name))
-    <h2>Name is not empty</h2>
-@endunless
-
-{{-- @if (!empty($name))
-    <h2>Name is not</h2>
-@endif --}}
-
-@empty(!$name)
-    <h2>Name is empty</h2>
-@endempty
-
-@isset($name)
-    <h2>Name has been set</h2>
-@endisset
+@switch($name)
+    @case('Ali')
+        <h2>Name is Ali!</h2>
+        @break
+    @case('Aliya')
+        <h2>Name is Aliya !</h2>
+        @break
+    @case('John')
+        <h2>Name is John!</h2>
+        @break    
+    @default
+        <h2>No Match Found</h2>
+        
+@endswitch
