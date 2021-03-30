6587eba52c62e63af1698f959c4c06d9c30d4806
Nslkh <olluzran@gmail.com>
2021-03-30 14:56:30 +0500

    @yield , @extends usage with example

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get(
-    '/products',
-    [ProductsController::class, 'index']
-)->name('products');
+Route::get('/', [PagesController::class, 'index']);
+
+Route::get('/about', [PagesController::class, 'about']);
