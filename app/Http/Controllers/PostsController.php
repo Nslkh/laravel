d68696e08e101c1810f98274996b3690acb9c3a0
Nslkh <olluzran@gmail.com>
2021-03-31 15:09:13 +0500

    Query Builder

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,20 +4,64 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Psy\Command\WhereamiCommand;
 
 class PostsController extends Controller
 {
     public function index()
     {
-        // Non fluent
-        // DB::select('select(['table' => 'posts', 'where' => ['id' => 1]])
+        $id = 3;
 
-        // Fluent
-        // DB::table('posts')->where('id', 1)->get();
+        // $posts = DB::table('posts')
+        // ->where('id', $id)
+        // ->get();
+
         
-        // $posts = DB::select('select * from posts where id = 3');
-        // $posts = DB::select('select * from posts where id = ?', [3]);
-        $posts = DB::select('select * from posts where id = :id', ['id' => 3]);
+        // $posts = DB::table('posts')
+        // ->select('body')
+        // ->get();
+
+        // $posts = DB::table('posts')
+        // ->where('created_at', '>', now()->subDay())
+        // ->orWhere('title', 'Mr.')
+        // ->get();
+
+        $posts = DB::table('posts')
+        // ->whereBetween('id', [3,5])
+        // ->whereNotNull('title')
+        
+        // ->select('title')
+        // ->distinct()
+
+        // ->orderBy('title', 'asc')
+        // ->oldest()
+        // ->inRandomOrder()
+        
+        // ->get();
+
+        // ->orderBy('created_at', 'desc')
+        // ->first();
+
+        // ->where('id', $id)
+        // ->count();
+
+        // ->min('id');
+        // ->max('id');
+        // ->sum('id');
+        // ->avg('id');
+
+        // ->insert([
+        //     'title' => 'New Post', 
+        //     'body' => "New Body"
+        // ]);
+
+        ->where('id', '=', 3)
+        // ->update([
+        //     'title' => 'New Title',
+        //     'body' => 'Updated Body'
+        // ]);
+
+        ->delete();
         dd($posts);
     }
 }
