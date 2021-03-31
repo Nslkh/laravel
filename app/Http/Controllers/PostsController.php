242753f38f68a64771c569f4095c77b6cd945d95
Nslkh <olluzran@gmail.com>
2021-03-31 14:31:45 +0500

    Query Builder

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostsController extends Controller
 {
     public function index()
     {
-        return view('posts/index');
+        // Non fluent
+        // DB::select('select(['table' => 'posts', 'where' => ['id' => 1]])
+
+        // Fluent
+        // DB::table('posts')->where('id', 1)->get();
+        
+        // $posts = DB::select('select * from posts where id = 3');
+        // $posts = DB::select('select * from posts where id = ?', [3]);
+        $posts = DB::select('select * from posts where id = :id', ['id' => 3]);
+        dd($posts);
     }
 }
